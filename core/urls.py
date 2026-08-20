465b454e85a4f311fc654a8efaf8164364a29660
christoph Hartig <christoph.hartig@gmail.com>
2026-08-20 10:14:04 +0200

    add account urls and adjust project urls

--- core/urls.py
+++ core/urls.py
@@ -22,7 +22,8 @@ from django.urls import include, path
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("projects/", include("projects.urls")),
+    path("account/", include("django.contrib.auth.urls")),
+    path("", include("projects.urls")),
     path("", include("pages.urls")),
 ]
 
